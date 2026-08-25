a2aa8951eb0ee5739662ac9fda7a57b2a5162e07
singh-prithvi <276057030+singh-prithvi@users.noreply.github.com>
2026-08-25 20:33:54 +0530

    changed search toggle from super only to super + leader key

--- .config/hypr/custom/keybinds.lua
+++ .config/hypr/custom/keybinds.lua
@@ -9,10 +9,8 @@ local qsIsAlive = qsIpcCall .. " TEST_ALIVE"
 --#############################################################################
 --## LAUNCHER & SHELL
 --#############################################################################
-hl.bind("SUPER + SUPER_L", hl.dsp.global("quickshell:searchToggleRelease"), { description = "Shell: Toggle search" })
-hl.bind("SUPER + SUPER_R", hl.dsp.global("quickshell:searchToggleRelease"))
-hl.bind("SUPER + SUPER_L", hl.dsp.exec_cmd(qsIsAlive .. " || pkill fuzzel || fuzzel"))
-hl.bind("SUPER + SUPER_R", hl.dsp.exec_cmd(qsIsAlive .. " || pkill fuzzel || fuzzel"))
+hl.bind("SUPER + SPACE", hl.dsp.global("quickshell:searchToggle"), { description = "Shell: Toggle search" })
+hl.bind("SUPER + SPACE", hl.dsp.exec_cmd(qsIsAlive .. " || pkill fuzzel || fuzzel"))
 
 hl.bind("SUPER_L", hl.dsp.global("quickshell:workspaceNumber"), { ignore_mods = true, transparent = true })
 hl.bind("SUPER_R", hl.dsp.global("quickshell:workspaceNumber"), { ignore_mods = true, transparent = true })
